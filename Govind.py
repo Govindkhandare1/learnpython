b0c5454ed947e92f8dabbafdac0c75508927d5d5
GOVIND KHANDARE <govindkhandare@govinds-macbook-air.local>
2023-07-29 23:18:29 +0530

    third time upload

--- Govind.py
+++ Govind.py
@@ -16,17 +16,18 @@ print (5**2) # Find out power of given number
 #Variable name contain numbers but cant start with numbers
 
 #1 variable name should start with alphabets
- ten = 10 #Allowed
-ten_number =10 #Allowed
-tennumber =10 #Allowed
+ #ten = 10 #Allowed
+#ten_number =10 #Allowed
+#tennumber =10 #Allowed
 
 # 2. variable name should not contain any special characters like ^%$@!&*()
-ten* =10 #not allowed invalid syntax
-ten%number=10 #not allowed invalid syntax
+#ten* =10 #not allowed invalid syntax
+#ten%number=10 #not allowed invalid syntax
 
 # 3. Variable name contain numbers but cant start with numbers
-ten = 10 #Allowed
-10ten =10 # Not allowed
-__num = 10 # Allowed # Hidden variable #private variable use underscore
+#ten = 10 #Allowed
+#10ten =10 # Not allowed
+#__num = 10 # Allowed # Hidden variable #private variable use underscore
 
 
+#this is govind khandare
